d36c18e00bed176199d9986f97c8e01a33a50af7
Dirk Arnez <smalldirkalex@gmail.com>
2024-10-05 22:36:06 +0800

    - update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const pdfjsLib = require("pdfjs-dist");
+// Create context
+var width   = 64
+var height  = 64
+var gl = require('gl')(width, height, { preserveDrawingBuffer: true })
 
-const PAGE_NUMBER = 1;
+//Clear screen to red
+gl.clearColor(1, 0, 0, 1)
+gl.clear(gl.COLOR_BUFFER_BIT)
 
-pdfjsLib
-.getDocument("ctn.pdf").promise
-.then(pdfDocument => pdfDocument.getPage(PAGE_NUMBER))
-.then(page => page.getTextContent())
-.then(textContent => console.log(textContent.items.map(item => item.str).join("")))
-.catch(e => {
-    console.error(e);
-});
+//Write output as a PPM formatted image
+var pixels = new Uint8Array(width * height * 4)
+gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+process.stdout.write(['P3\n# gl.ppm\n', width, " ", height, '\n255\n'].join(''))
+
+for(var i = 0; i < pixels.length; i += 4) {
+  for(var j = 0; j < 3; ++j) {
+    process.stdout.write(pixels[i + j] + ' ')
+  }
+}
